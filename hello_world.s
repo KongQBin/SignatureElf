b85dc4c805927012a7d0ac98ca996acfc4d0b911
KongBin <51024054@QQ.com>
2023-08-14 00:23:27 +0800

    update(all):兼容 Linux 5.4.0-150-generic #167~18.04.1-Ubuntu

--- hello_world.s
+++ hello_world.s
@@ -1,6 +1,6 @@
 .data				 # 数据段声明
 
-	msg:   .ascii  "hello, world\n"
+	msg:   .ascii  "good run!\n"
 	len= .-msg
 	filename:  .ascii  "output.txt"
 
@@ -10,25 +10,29 @@
 
 _start:
 
-    pushq %rax
-    pushq %rbx
-    pushq %rcx
-    pushq %rdx
-    movq $5, %rax
-    movq $filename, %rbx
-    movq $03101, %rcx
-    movq $0666, %rdx
-    int $0x80		# 调用内核功能
-    movq %rax,%rbx
-    movq $4, %rax	#系统调用号(sys_write) 
-    movq $msg, %rcx #要写入的字符串
-    movq $len, %rdx #字符串长度
-    int  $0x80 	    #调用内核功能
-    popq %rdx
-    popq %rcx
-    popq %rbx
-    popq %rax
+    push %rax
+    push %rbx
+    push %rcx
+    push %rdx
+#    mov $257, %rax
+#    mov $-100, %rdi
+#    lea 0x0(%rip), %rsi
+#    mov $03101, %rdx
+#    mov $0666, %r10
+#    syscall
+#    mov %rax,%rbx
+    mov $1, %rax	#系统调用号(sys_write) 
+    mov $1, %rdi	#系统调用号(sys_write) 
+    lea 0x0(%rip), %rsi #要写入的字符串
+    mov $len, %rdx #字符串长度
+    syscall
+    pop %rdx
+    pop %rcx
+    pop %rbx
+    pop %rax
+    lea -0x3dd(%rip), %rdi
+    jmp *%rdi
     				# 退出程序
-    movl $1, %eax	# 系统调用号(sys_exit) 
+    movl $60, %eax	# 系统调用号(sys_exit) 
     movl $0, %ebx	# 参数一：退出代码
-    int $0x80		# 调用内核功能
+    syscall
